92335182a5dda4f0806bc21c02e65848aa3c3901
qgymje <chaingaoming86@gmail.com>
2016-07-31 11:26:49 +0800

    json tag test

--- services/runs/create_test.go
+++ services/runs/create_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestLocaitons(t *testing.T) {
 	locs := `[
-    {"latitude":31.003800, "longitude":121.223080, "altitude": 5.2, "timestamp": "2016-07-28T17:49:09.088899+08:00"},
+    {"latitude":31.103800, "longitude":121.223080, "altitude": 5.2, "timestamp": "2016-07-28T17:49:09.088899+08:00"},
     {"latitude":31.203800, "longitude":121.233080, "alttitude": 5.2, "timestams": "2016-07-28T17:52:09.088899+08:00"},
     {"latitude":31.403800, "longitude":121.243080, "altitude": 5.2, "timestamp": "2016-07-28T17:55:09.088899+08:00"}
    ]`
