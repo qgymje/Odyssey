00213b40f6890f6126de0eabcd7eea200285885a
qgymje <chaingaoming86@gmail.com>
2016-08-02 17:40:50 +0800

    mv main.go to api_services

--- models/run_comments.go
+++ models/run_comments.go
@@ -4,14 +4,15 @@ import "time"
 
 // RunComment model 表示对一次跑步纪录的评论
 type RunComment struct {
-	ID              int64     `json:"run_comment_id"`
-	RunID           int       `json:"run_id"`
-	CommentUserID   int       `db:"comment_user_id" json:"comment_user"`
-	ParentCommentID int64     `db:"reply_comment" json:"reply_comment"` // 如果为空, 则表示对跑步纪录的评论, 不为空, 则为对用户的评论的评论
-	CreatedAt       time.Time `db:"created_at" json:"created_at"`
+	ID              int64 `json:"run_comment_id"`
+	RunID           int64 `db:"run_id" json:"run_id"`
+	UserID          int64 `db:"user_id" json:"user_id"`
+	ParentCommentID int64 `db:"parent_comment_id" json:"parent_comment_id"` // 如果为空, 则表示对跑步纪录的评论, 不为空, 则为对用户的评论的评论
 
-	CommentUser   *User
-	ParentComment *RunComment
+	Content   string    `json:"conente"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+
+	CommentUser *User
 }
 
 // Create 创建一个评论
--- servers/api_services/api_services.go
+++ servers/api_services/api_services.go
@@ -24,7 +24,7 @@ func initEnv() {
 
 func init() {
 	initEnv()
-	utils.InitConfig("./configs/")
+	utils.InitConfig("../../configs/")
 	utils.InitLogger()
 	utils.InitRander()
 	models.InitModels()
@@ -44,15 +44,55 @@ func main() {
 		v1.POST("/login", user.Login)
 		v1.DELETE("/logout", user.Logout)
 
+		/*
+			v1.Get("/profile", user.Profile)
+			v1.Get("/user/games", user.Games)
+			v1.Get("/user/friends", user.Friends)
+			v1.Get("/user/Groups", user.Groups)
+		*/
+
+		/*
+			group := new(controllers.Group)
+			v1.Get("/group", group.Index)
+			v1.Get("/group/:group_id", group.Show)
+			v1.Post("/group", group.Create)
+			v1.Post("/group/:group_id/join", group.Join)
+			v1.Delete("/group/:group_id/quit", group.Quit)
+		*/
+
 		run := new(controllers.Run)
 		v1.POST("/run/:user_id", run.Create)
 		v1.GET("/run/:user_id", run.Index)
 		v1.GET("/run/:user_id/:run_id", run.Show)
 
+		/*
+			runComment := new(controller.RunComment)
+			v1.Get("/run/comment", runComment.Show)
+			v1.Post("/run/comment/:run_id", runComment.Comment)
+			v1.Post("/run/comment/:run_id/reply/:comment_id", runComment.Reply)
+		*/
+
 		feedback := new(controllers.Feedback)
 		v1.GET("/feedback", feedback.Index)
 		v1.POST("/feedback", feedback.Create)
 		v1.POST("/feedback/:feedback_id/reply", feedback.Reply)
+
+		/*
+			game := new(controllers.Game)
+			v1.Get("/game", game.Index)
+			v1.Get("/game/:game_id", game.Show)
+			v1.Post("/game", game.Create)
+			v1.Put("/game/edit", game.Update)
+			v1.Delete("/game", game.Destroy)
+		*/
+
+		/*
+			register := new(controllers.Register)
+			v1.Post("/register/:game_id", register.Create)
+			v1.Post("/register/:game_id/pay", register.Pay)
+			v1.Post("/register/:game_id/cancel", register.PayCancel)
+			v1.Post("/register/:game_id/refund", register.PayRefund)
+		*/
 
 	}
 
